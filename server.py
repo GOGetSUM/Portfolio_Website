95f7824f9f44fde6932b501916f839fd112d8bd8
Nick <testcorona0429@gmail.com>
2022-07-06 22:40:52 -0700

    Updated most of AGM documentation. Missing video 2 actuation and setup.

--- server.py
+++ server.py
@@ -65,10 +65,16 @@ def pdftoaudio():
 @app.route('/wscrapper',methods=["GET", "POST"])
 def wscrapper():
     return render_template('webscrapper.html')
+
 # Certs_ Index ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @app.route('/certs',methods=["GET", "POST"])
 def certs():
     return render_template('certs.html')
+
+# Automated Garden Mister ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@app.route('/AGM',methods=["GET", "POST"])
+def AGM():
+    return render_template('AGM.html')
 
 
 #--------run app------------------------
